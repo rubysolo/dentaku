3e4231ccda64152c9d3937ae75938d285d9a3eaa
Solomon White <rubysolo@gmail.com>
2013-02-21 06:54:18 -0700

    move rule pattern definition into a separate class

--- lib/dentaku/evaluator.rb
+++ lib/dentaku/evaluator.rb
@@ -1,68 +1,7 @@
-require 'dentaku/token'
-require 'dentaku/token_matcher'
+require 'dentaku/rules'
 
 module Dentaku
   class Evaluator
-    # tokens
-    T_NUMERIC    = TokenMatcher.new(:numeric)
-    T_STRING     = TokenMatcher.new(:string)
-    T_ADDSUB     = TokenMatcher.new(:operator, [:add, :subtract])
-    T_MULDIV     = TokenMatcher.new(:operator, [:multiply, :divide])
-    T_POW        = TokenMatcher.new(:operator, :pow)
-    T_COMPARATOR = TokenMatcher.new(:comparator)
-    T_COMP_GT    = TokenMatcher.new(:comparator, [:gt, :ge])
-    T_COMP_LT    = TokenMatcher.new(:comparator, [:lt, :le])
-    T_OPEN       = TokenMatcher.new(:grouping, :open)
-    T_CLOSE      = TokenMatcher.new(:grouping, :close)
-    T_COMMA      = TokenMatcher.new(:grouping, :comma)
-    T_NON_GROUP  = TokenMatcher.new(:grouping).invert
-    T_LOGICAL    = TokenMatcher.new(:logical)
-    T_COMBINATOR = TokenMatcher.new(:combinator)
-    T_IF         = TokenMatcher.new(:function, :if)
-    T_ROUND      = TokenMatcher.new(:function, :round)
-    T_ROUNDUP    = TokenMatcher.new(:function, :roundup)
-    T_ROUNDDOWN  = TokenMatcher.new(:function, :rounddown)
-    T_NOT        = TokenMatcher.new(:function, :not)
-
-    T_NON_GROUP_STAR = TokenMatcher.new(:grouping).invert.star
-
-    # patterns
-    P_GROUP      = [T_OPEN,    T_NON_GROUP_STAR, T_CLOSE]
-    P_MATH_ADD   = [T_NUMERIC, T_ADDSUB,         T_NUMERIC]
-    P_MATH_MUL   = [T_NUMERIC, T_MULDIV,         T_NUMERIC]
-    P_MATH_POW   = [T_NUMERIC, T_POW,            T_NUMERIC]
-    P_RANGE_ASC  = [T_NUMERIC, T_COMP_LT,        T_NUMERIC, T_COMP_LT, T_NUMERIC]
-    P_RANGE_DESC = [T_NUMERIC, T_COMP_GT,        T_NUMERIC, T_COMP_GT, T_NUMERIC]
-    P_NUM_COMP   = [T_NUMERIC, T_COMPARATOR,     T_NUMERIC]
-    P_STR_COMP   = [T_STRING,  T_COMPARATOR,     T_STRING]
-    P_COMBINE    = [T_LOGICAL, T_COMBINATOR,     T_LOGICAL]
-
-    P_IF         = [T_IF, T_OPEN, T_NON_GROUP, T_COMMA, T_NON_GROUP, T_COMMA, T_NON_GROUP, T_CLOSE]
-    P_ROUND_ONE  = [T_ROUND, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-    P_ROUND_TWO  = [T_ROUND, T_OPEN, T_NON_GROUP_STAR, T_COMMA, T_NUMERIC, T_CLOSE]
-    P_ROUNDUP    = [T_ROUNDUP, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-    P_ROUNDDOWN  = [T_ROUNDDOWN, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-    P_NOT        = [T_NOT, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-
-    RULES = [
-      [P_IF,         :if],
-      [P_ROUND_ONE,  :round],
-      [P_ROUND_TWO,  :round],
-      [P_ROUNDUP,    :roundup],
-      [P_ROUNDDOWN,  :rounddown],
-      [P_NOT,        :not],
-
-      [P_GROUP,      :evaluate_group],
-      [P_MATH_POW,   :apply],
-      [P_MATH_MUL,   :apply],
-      [P_MATH_ADD,   :apply],
-      [P_RANGE_ASC,  :expand_range],
-      [P_RANGE_DESC, :expand_range],
-      [P_NUM_COMP,   :apply],
-      [P_STR_COMP,   :apply],
-      [P_COMBINE,    :apply]
-    ]
-
     def evaluate(tokens)
       evaluate_token_stream(tokens).value
     end
@@ -70,7 +9,7 @@ module Dentaku
     def evaluate_token_stream(tokens)
       while tokens.length > 1
         matched = false
-        RULES.each do |pattern, evaluator|
+        Rules.each do |pattern, evaluator|
           pos, match = find_rule_match(pattern, tokens)
 
           if pos
