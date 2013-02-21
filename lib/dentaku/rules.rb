9f0d311bb6aaede49781782d65e3f348c86a5f92
Solomon White <rubysolo@gmail.com>
2013-02-21 16:48:00 -0700

    move pattern matching definitions into methods

--- lib/dentaku/rules.rb
+++ lib/dentaku/rules.rb
@@ -3,68 +3,85 @@ require 'dentaku/token_matcher'
 
 module Dentaku
   class Rules
-    # tokens
-    T_NUMERIC    = TokenMatcher.new(:numeric)
-    T_STRING     = TokenMatcher.new(:string)
-    T_ADDSUB     = TokenMatcher.new(:operator, [:add, :subtract])
-    T_MULDIV     = TokenMatcher.new(:operator, [:multiply, :divide])
-    T_POW        = TokenMatcher.new(:operator, :pow)
-    T_COMPARATOR = TokenMatcher.new(:comparator)
-    T_COMP_GT    = TokenMatcher.new(:comparator, [:gt, :ge])
-    T_COMP_LT    = TokenMatcher.new(:comparator, [:lt, :le])
-    T_OPEN       = TokenMatcher.new(:grouping, :open)
-    T_CLOSE      = TokenMatcher.new(:grouping, :close)
-    T_COMMA      = TokenMatcher.new(:grouping, :comma)
-    T_NON_GROUP  = TokenMatcher.new(:grouping).invert
-    T_LOGICAL    = TokenMatcher.new(:logical)
-    T_COMBINATOR = TokenMatcher.new(:combinator)
-    T_IF         = TokenMatcher.new(:function, :if)
-    T_ROUND      = TokenMatcher.new(:function, :round)
-    T_ROUNDUP    = TokenMatcher.new(:function, :roundup)
-    T_ROUNDDOWN  = TokenMatcher.new(:function, :rounddown)
-    T_NOT        = TokenMatcher.new(:function, :not)
-
-    T_NON_GROUP_STAR = TokenMatcher.new(:grouping).invert.star
-
-    # patterns
-    P_GROUP      = [T_OPEN,    T_NON_GROUP_STAR, T_CLOSE]
-    P_MATH_ADD   = [T_NUMERIC, T_ADDSUB,         T_NUMERIC]
-    P_MATH_MUL   = [T_NUMERIC, T_MULDIV,         T_NUMERIC]
-    P_MATH_POW   = [T_NUMERIC, T_POW,            T_NUMERIC]
-    P_RANGE_ASC  = [T_NUMERIC, T_COMP_LT,        T_NUMERIC, T_COMP_LT, T_NUMERIC]
-    P_RANGE_DESC = [T_NUMERIC, T_COMP_GT,        T_NUMERIC, T_COMP_GT, T_NUMERIC]
-    P_NUM_COMP   = [T_NUMERIC, T_COMPARATOR,     T_NUMERIC]
-    P_STR_COMP   = [T_STRING,  T_COMPARATOR,     T_STRING]
-    P_COMBINE    = [T_LOGICAL, T_COMBINATOR,     T_LOGICAL]
-
-    P_IF         = [T_IF, T_OPEN, T_NON_GROUP, T_COMMA, T_NON_GROUP, T_COMMA, T_NON_GROUP, T_CLOSE]
-    P_ROUND_ONE  = [T_ROUND, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-    P_ROUND_TWO  = [T_ROUND, T_OPEN, T_NON_GROUP_STAR, T_COMMA, T_NUMERIC, T_CLOSE]
-    P_ROUNDUP    = [T_ROUNDUP, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-    P_ROUNDDOWN  = [T_ROUNDDOWN, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-    P_NOT        = [T_NOT, T_OPEN, T_NON_GROUP_STAR, T_CLOSE]
-
     def self.each
       @rules ||= [
-        [P_IF,         :if],
-        [P_ROUND_ONE,  :round],
-        [P_ROUND_TWO,  :round],
-        [P_ROUNDUP,    :round_int],
-        [P_ROUNDDOWN,  :round_int],
-        [P_NOT,        :not],
+        [ p(:if),         :if             ],
+        [ p(:round_one),  :round          ],
+        [ p(:round_two),  :round          ],
+        [ p(:roundup),    :round_int      ],
+        [ p(:rounddown),  :round_int      ],
+        [ p(:not),        :not            ],
 
-        [P_GROUP,      :evaluate_group],
-        [P_MATH_POW,   :apply],
-        [P_MATH_MUL,   :apply],
-        [P_MATH_ADD,   :apply],
-        [P_RANGE_ASC,  :expand_range],
-        [P_RANGE_DESC, :expand_range],
-        [P_NUM_COMP,   :apply],
-        [P_STR_COMP,   :apply],
-        [P_COMBINE,    :apply]
+        [ p(:group),      :evaluate_group ],
+        [ p(:math_pow),   :apply          ],
+        [ p(:math_mul),   :apply          ],
+        [ p(:math_add),   :apply          ],
+        [ p(:range_asc),  :expand_range   ],
+        [ p(:range_desc), :expand_range   ],
+        [ p(:num_comp),   :apply          ],
+        [ p(:str_comp),   :apply          ],
+        [ p(:combine),    :apply          ]
       ]
 
       @rules.each { |r| yield r }
+    end
+
+    def self.t(name)
+      @matchers ||= {
+        numeric:        TokenMatcher.new(:numeric),
+        string:         TokenMatcher.new(:string),
+        addsub:         TokenMatcher.new(:operator, [:add, :subtract]),
+        muldiv:         TokenMatcher.new(:operator, [:multiply, :divide]),
+        pow:            TokenMatcher.new(:operator, :pow),
+        comparator:     TokenMatcher.new(:comparator),
+        comp_gt:        TokenMatcher.new(:comparator, [:gt, :ge]),
+        comp_lt:        TokenMatcher.new(:comparator, [:lt, :le]),
+        open:           TokenMatcher.new(:grouping, :open),
+        close:          TokenMatcher.new(:grouping, :close),
+        comma:          TokenMatcher.new(:grouping, :comma),
+        logical:        TokenMatcher.new(:logical),
+        combinator:     TokenMatcher.new(:combinator),
+        if:             TokenMatcher.new(:function, :if),
+        round:          TokenMatcher.new(:function, :round),
+        roundup:        TokenMatcher.new(:function, :roundup),
+        rounddown:      TokenMatcher.new(:function, :rounddown),
+        not:            TokenMatcher.new(:function, :not),
+        non_group:      TokenMatcher.new(:grouping).invert,
+        non_group_star: TokenMatcher.new(:grouping).invert.star
+      }
+
+      @matchers[name]
+    end
+
+    def self.p(name)
+      @patterns ||= {
+        group:      pattern(:open,    :non_group_star, :close),
+        math_add:   pattern(:numeric, :addsub,         :numeric),
+        math_mul:   pattern(:numeric, :muldiv,         :numeric),
+        math_pow:   pattern(:numeric, :pow,            :numeric),
+        range_asc:  pattern(:numeric, :comp_lt,        :numeric,  :comp_lt, :numeric),
+        range_desc: pattern(:numeric, :comp_gt,        :numeric,  :comp_gt, :numeric),
+        num_comp:   pattern(:numeric, :comparator,     :numeric),
+        str_comp:   pattern(:string,  :comparator,     :string),
+        combine:    pattern(:logical, :combinator,     :logical),
+
+        if:         func_pattern(:if,        :non_group,      :comma, :non_group, :comma, :non_group),
+        round_one:  func_pattern(:round,     :non_group_star),
+        round_two:  func_pattern(:round,     :non_group_star, :comma, :numeric),
+        roundup:    func_pattern(:roundup,   :non_group_star),
+        rounddown:  func_pattern(:rounddown, :non_group_star),
+        not:        func_pattern(:not,       :non_group_star)
+      }
+
+      @patterns[name]
+    end
+
+    def self.pattern(*symbols)
+      symbols.map { |s| t(s) }
+    end
+
+    def self.func_pattern(func, *tokens)
+      pattern(func, :open, *tokens, :close)
     end
   end
 end
