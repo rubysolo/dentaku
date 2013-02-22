f86559ab1247c85443c4dab7b51265be37332a32
Solomon White <rubysolo@gmail.com>
2013-02-22 07:22:48 -0700

    split up tokenize into separate methods

--- lib/dentaku/tokenizer.rb
+++ lib/dentaku/tokenizer.rb
@@ -8,31 +8,36 @@ module Dentaku
     RPAREN = TokenMatcher.new(:grouping, :close)
 
     def tokenize(string)
-      nesting = 0
-      tokens  = []
-      input   = string.dup
+      @nesting = 0
+      @tokens  = []
+      input    = string.dup
 
       until input.empty?
         raise "parse error at: '#{ input }'" unless TokenScanner.scanners.any? do |scanner|
-          if token = scanner.scan(input)
-            raise "unexpected zero-width match (:#{ token.category }) at '#{ input }'" if token.length == 0
-
-            nesting += 1 if LPAREN == token
-            nesting -= 1 if RPAREN == token
-            raise "too many closing parentheses" if nesting < 0
-
-            tokens << token unless token.is?(:whitespace)
-            input.slice!(0, token.length)
-
-            true
-          else
-            false
-          end
+          scanned, input = scan(input, scanner)
+          scanned
         end
       end
 
-      raise "too many opening parentheses" if nesting > 0
-      tokens
+      raise "too many opening parentheses" if @nesting > 0
+
+      @tokens
+    end
+
+    def scan(string, scanner)
+      if token = scanner.scan(string)
+        raise "unexpected zero-width match (:#{ token.category }) at '#{ string }'" if token.length == 0
+
+        @nesting += 1 if LPAREN == token
+        @nesting -= 1 if RPAREN == token
+        raise "too many closing parentheses" if @nesting < 0
+
+        @tokens << token unless token.is?(:whitespace)
+
+        [true, string[token.length..-1]]
+      else
+        [false, string]
+      end
     end
   end
 end
