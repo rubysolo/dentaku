7d078af8c338acb860122bb29b53fe6b0d54215e
Toms Mikoss <toms.mikoss@gmail.com>
2016-01-04 13:29:17 +0200

    Allow ROUNDUP & ROUNDDOWN functions to accept precision argument like in MS Office

--- lib/dentaku/ast/functions/rounddown.rb
+++ lib/dentaku/ast/functions/rounddown.rb
@@ -1,5 +1,10 @@
 require_relative '../function'
 
-Dentaku::AST::Function.register(:rounddown, :numeric, ->(numeric) {
-  numeric.floor
+Dentaku::AST::Function.register(:rounddown, :numeric, ->(numeric, precision=0) {
+  if precision == 0 # Ensure int is returned
+    numeric.floor
+  else
+    tens = 10.0**precision
+    (numeric * tens).floor / tens
+  end
 })
--- lib/dentaku/ast/functions/roundup.rb
+++ lib/dentaku/ast/functions/roundup.rb
@@ -1,5 +1,10 @@
 require_relative '../function'
 
-Dentaku::AST::Function.register(:roundup, :numeric, ->(numeric) {
-  numeric.ceil
+Dentaku::AST::Function.register(:roundup, :numeric, ->(numeric, precision=0) {
+  if precision == 0 # Ensure int is returned
+    numeric.ceil
+  else
+    tens = 10.0**precision
+    (numeric * tens).ceil / tens
+  end
 })
