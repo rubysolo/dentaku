562823ef3352a2470fab9700b696d80037b02f89
William Carey <willtcarey@gmail.com>
2017-01-27 14:25:50 -0600

    Allow arithmetic operands that simply respond to the arthmetic operation

--- lib/dentaku/ast/arithmetic.rb
+++ lib/dentaku/ast/arithmetic.rb
@@ -16,6 +16,10 @@ module Dentaku
         :numeric
       end
 
+      def operator
+        raise "Not implemented"
+      end
+
       def value(context={})
         l = cast(left.value(context))
         r = cast(right.value(context))
@@ -25,7 +29,7 @@ module Dentaku
       private
 
       def cast(value, prefer_integer=true)
-        validate_numeric(value)
+        validate_operation(value)
         v = BigDecimal.new(value, Float::DIG+1)
         v = v.to_i if prefer_integer && v.frac.zero?
         v
@@ -38,10 +42,10 @@ module Dentaku
         node && (node.dependencies.any? || node.type == :numeric)
       end
 
-      def validate_numeric(value)
-        Float(value)
-      rescue ::ArgumentError, ::TypeError
-        fail Dentaku::ArgumentError, "#{ self.class } requires numeric operands"
+      def validate_operation(value)
+        unless value.respond_to?(operator)
+          fail Dentaku::ArgumentError, "#{ self.class } requires operands that respond to #{operator}"
+        end
       end
     end
 
@@ -76,6 +80,10 @@ module Dentaku
     end
 
     class Division < Arithmetic
+      def operator
+        :/
+      end
+
       def value(context={})
         r = cast(right.value(context), false)
         raise Dentaku::ZeroDivisionError if r.zero?
