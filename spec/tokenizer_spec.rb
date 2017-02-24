e4d77b7cca0e96a8e5b5ca5a375c2d83ba9754b8
Christopher Locke <project.eutopia@gmail.com>
2017-02-23 18:21:34 -0800

    Time literals can contain timezone information

--- spec/tokenizer_spec.rb
+++ spec/tokenizer_spec.rb
@@ -172,16 +172,18 @@ describe Dentaku::Tokenizer do
   end
 
   it 'tokenizes Time literals' do
-    tokens = tokenizer.tokenize('2017-01-01 2017-01-2 2017-1-03 2017-01-04 12:23:42 2017-1-5 1:2:3 2017-1-06 1:02:30')
-    expect(tokens.length).to eq(6)
-    expect(tokens.map(&:category)).to eq([:datetime, :datetime, :datetime, :datetime, :datetime, :datetime])
+    tokens = tokenizer.tokenize('2017-01-01 2017-01-2 2017-1-03 2017-01-04 12:23:42 2017-1-5 1:2:3 2017-1-06 1:02:30 2017-01-07 12:34:56 Z 2017-01-08 1:2:3 +0800')
+    expect(tokens.length).to eq(8)
+    expect(tokens.map(&:category)).to eq([:datetime, :datetime, :datetime, :datetime, :datetime, :datetime, :datetime, :datetime])
     expect(tokens.map(&:value)).to eq([
       Time.local(2017, 1, 1).to_datetime,
       Time.local(2017, 1, 2).to_datetime,
       Time.local(2017, 1, 3).to_datetime,
       Time.local(2017, 1, 4, 12, 23, 42).to_datetime,
       Time.local(2017, 1, 5, 1, 2, 3).to_datetime,
-      Time.local(2017, 1, 6, 1, 2, 30).to_datetime
+      Time.local(2017, 1, 6, 1, 2, 30).to_datetime,
+      Time.utc(2017, 1, 7, 12, 34, 56).to_datetime,
+      Time.new(2017, 1, 8, 1, 2, 3, "+08:00").to_datetime
     ])
   end
 
