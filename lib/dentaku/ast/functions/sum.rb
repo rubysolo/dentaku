6b5c9586f9ffea06bcfd26671ed2268d78c97094
AndrewRPerkins <andrew.perkins@innowell.org>
2017-10-18 16:28:42 +1100

    add a variadic SUM function
    closes #140

---
+++ lib/dentaku/ast/functions/sum.rb
@@ -0,0 +1,12 @@
+require_relative '../function'
+
+Dentaku::AST::Function.register(:sum, :numeric, ->(*args) {
+  if args.empty?
+    raise Dentaku::ArgumentError.for(
+        :too_few_arguments,
+        function_name: 'SUM()', at_least: 1, given: 0
+    ), 'SUM() requires at least one argument'
+  end
+
+  args.map { |arg| Dentaku::AST::Function.numeric(arg) }.reduce(0, :+)
+})
