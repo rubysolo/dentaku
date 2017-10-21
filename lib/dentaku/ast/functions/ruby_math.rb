5c3ee1a5d8d5ea2e459e04dfe3649b2f6d592e8f
Nathan Broadbent <git@ndbroadbent.com>
2017-10-21 22:21:03 +0700

    Flatten all arguments to min, max, sum, and Math functions, so that they can be called with arrays
    closes #144

--- lib/dentaku/ast/functions/ruby_math.rb
+++ lib/dentaku/ast/functions/ruby_math.rb
@@ -3,6 +3,6 @@ require_relative '../function'
 
 Math.methods(false).each do |method|
   Dentaku::AST::Function.register(method, :numeric, lambda { |*args|
-    Math.send(method, *args.map { |arg| Dentaku::AST::Function.numeric(arg) })
+    Math.send(method, *args.flatten.map { |arg| Dentaku::AST::Function.numeric(arg) })
   })
 end
