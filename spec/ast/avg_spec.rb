28d230360660bda9394d46770b9b8b53213bf1dd
FENG Zhichao <flankerfc@gmail.com>
2018-01-23 08:57:14 +0800

    Support AVG (average) function.

---
+++ spec/ast/avg_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require 'dentaku/ast/functions/avg'
+require 'dentaku'
+
+describe 'Dentaku::AST::Function::Avg' do
+  it 'returns the average of an array of Numeric values' do
+    result = Dentaku('AVG(1, x, 1.8)', x: 2.3)
+    expect(result).to eq 1.7
+  end
+
+  it 'returns the average of a single entry array of a Numeric value' do
+    result = Dentaku('AVG(x)', x: 2.3)
+    expect(result).to eq 2.3
+  end
+
+  it 'returns the average even if a String is passed' do
+    result = Dentaku('AVG(1, x, 1.8)', x: '2.3')
+    expect(result).to eq 1.7
+  end
+
+  it 'returns the average even if an array is passed' do
+    result = Dentaku('AVG(1, x, 2.3)', x: [4, 5])
+    expect(result).to eq 3.075
+  end
+
+  context 'checking errors' do
+    let(:calculator) { Dentaku::Calculator.new }
+
+    it 'raises an error if no arguments are passed' do
+      expect { calculator.evaluate!('AVG()') }.to raise_error(ArgumentError)
+    end
+  end
+end
