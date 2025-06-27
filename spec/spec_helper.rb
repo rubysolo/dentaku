94e12c6b403dfe3466536d0c18b426a65e77cf3e
Ivan Kuchin <tadump+git@gmail.com>
2025-06-27 22:45:36 +0200

    disable minimum coverage by file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
 ])
 
 SimpleCov.minimum_coverage 90
-SimpleCov.minimum_coverage_by_file 80
+# SimpleCov.minimum_coverage_by_file 80
 
 SimpleCov.start do
   add_filter "spec/"
