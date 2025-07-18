819b38d7800a38ee24fc8b2f0290c31b27d61214
Ignatius Reza <lyoneil.de.sire@gmail.com>
2025-07-18 11:59:51 +0200

    Fix various issue with array arithmetic
    - should work with explicit arrays
    - should not crash when it has deeply a string that looks like date

--- spec/ast/addition_spec.rb
+++ spec/ast/addition_spec.rb
@@ -20,22 +20,21 @@ describe Dentaku::AST::Addition do
     expect(node.value).to eq(11)
   end
 
-  it 'requires numeric operands' do
+  it 'requires operands that respond to +' do
     expect {
-      described_class.new(five, t)
-    }.to raise_error(Dentaku::NodeError, /requires numeric operands/)
+      described_class.new(five, t).value
+    }.to raise_error(Dentaku::ArgumentError, /requires operands that respond to +/)
 
     expression = Dentaku::AST::Multiplication.new(five, five)
     group = Dentaku::AST::Grouping.new(expression)
 
     expect {
-      described_class.new(group, five)
+      described_class.new(group, five).value
     }.not_to raise_error
   end
 
   it 'allows operands that respond to addition' do
     # Sample struct that has a custom definition for addition
-
     Addable = Struct.new(:value) do
       def +(other)
         case other
@@ -51,12 +50,18 @@ describe Dentaku::AST::Addition do
     operand_six = Dentaku::AST::Numeric.new Dentaku::Token.new(:numeric, Addable.new(6))
 
     expect {
-      described_class.new(operand_five, operand_six)
+      described_class.new(operand_five, operand_six).value
     }.not_to raise_error
 
     expect {
-      described_class.new(operand_five, six)
+      described_class.new(operand_five, six).value
     }.not_to raise_error
+  end
 
+  it 'does not try to parse nested string as date' do
+    a = ['2017-01-01', '2017-01-02']
+    b = ['2017-01-01']
+
+    expect(Dentaku('a + b', a: a, b: b)).to eq(['2017-01-01', '2017-01-02', '2017-01-01'])
   end
 end
